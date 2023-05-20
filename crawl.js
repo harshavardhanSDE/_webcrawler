c2ed08fa938785310de16960a953648c71bbb0b1
harshavardhanSDE <harshavardhan.workspace@gmail.com>
2023-05-21 03:39:36 +0530

    modified: crawl.js, main.js

--- crawl.js
+++ crawl.js
@@ -1,19 +1,47 @@
 // jsdom for dom manipulation
 const { JSDOM } = require('jsdom'); 
+// pages: object
+async function crawl(baseURL, currentCrawlUrl, pages) {
+    
+    const baseURLobj = new URL(baseURL); 
+    const currentCrawlUrlObj = new URL(currentCrawlUrl)
+    // checking if the page is of same website.
+    if (currentCrawlUrlObj.hostname !== baseURLobj.hostname) {
+        return pages; 
+    }
+    // checking if the link already exits.
+    const normalizedURL = normalizeURL(currentCrawlUrlObj); 
+    if (pages[normalizedURL] > 0) {
+        pages[normalizedURL]++; 
+        return pages; 
+    }
+    // if the link is not present initialize it to 1. 
+    pages[normalizedURL] = 1; 
+    console.log(`crawling the site ${normalizedURL}`); 
 
-async function crawl(toCrawlUrl) {
-    console.log(`crawling the site ${toCrawlUrl}`); 
+    // checking for status error.
     try {
-        const response = await fetch(toCrawlUrl); 
+        const response = await fetch(currentCrawlUrl); 
         if (response.status > 399) {
-            console.log(response.status); 
-            return 
+            console.log(` Status error: code -> ${response.status}`); 
+            return pages
         }
-        console.log(await response.text())
+        // checking for type error. 
+        const responseType = response.headers.get('content-type'); 
+        if (!responseType.includes('text/html')) {
+            console.log(`Content error: received -> ${responseType}`); 
+            return pages; 
+        }
+        const htmlBody = await response.text(); 
+        const urlsFromBody = getURLsFromDom(htmlBody, baseURL); 
+        for (const nextUrl of urlsFromBody) {
+            pages = await crawl(baseURL, nextUrl, pages)
+        }
     }
     catch (err) {
         console.log(err.message); 
     }
+    return pages; 
 }
 
 function getURLsFromDom(htmlBody, baseURL) {
@@ -58,19 +86,19 @@ function normalizeURL(urlString) {
 
 }
 
-	const html = `
-	<html>
-		<body>
-			<a href="https://github.com/user/contributions/">
-			Contributions
-			</a>
-		</body>
-	</html>
-	`;
+	// const html = `
+	// <html>
+	// 	<body>
+	// 		<a href="https://github.com/user/contributions/">
+	// 		Contributions
+	// 		</a>
+	// 	</body>
+	// </html>
+	// `;
 
-//console.log(getURLsFromDom(html, "https://github.com")); 
+    //console.log(getURLsFromDom(html, "https://github.com")); 
 
-/* exporting the file as module to be used in other files. */
+    /* exporting the file as module to be used in other files. */
 module.exports = {
     normalizeURL, 
     getURLsFromDom, 
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawl } = require("./crawl.js");
 
-function main() {
+async function main() {
 	if (process.argv.length < 3) {
 		console.log("No input found! ");
 		process.exit(1);
@@ -12,8 +12,11 @@ function main() {
 		console.log("crawling started.");
 		const crawlURL = process.argv;
 		console.log(`crawling at ${crawlURL}`);
+		// Explantion: for ln.16 in dev-doc.md: ln.56
 		const baseURL = process.argv[2];
-		crawl(baseURL);
+		// the return type of crawl function is an "object"
+		const pages = await crawl(baseURL, baseURL, {});
+		console.log(pages);
 	}
 }
 main();
